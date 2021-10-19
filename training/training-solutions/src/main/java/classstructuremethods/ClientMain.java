a5a774691864b6c04c783372304738604fc7114c
GLTGRG <galatgergo@gmail.com>
2021-10-19 14:18:15 +0200

    methods first task completed

---
+++ training/training-solutions/src/main/java/classstructuremethods/ClientMain.java
@@ -0,0 +1,14 @@
+package classstructuremethods;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setName("Gergo Galat");
+        client.setYear(1992);
+        client.setAddress("1139 Budapest Üteg street 11/A");
+        System.out.println("Name:" + client.getName() + " " + "Year of birth:" + client.getYear() + " " + "Address:" + client.getAddress());
+        client.migrate("1078 Budapest Marek József steet 36");
+        System.out.println("New address: " + client.getAddress());
+    }
+}
