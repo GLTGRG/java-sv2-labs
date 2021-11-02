35e0fe280eb4841802449b1183910d78ab905b0f
GLTGRG <galatgergo@gmail.com>
2021-11-02 22:56:44 +0100

    debug feladatok

---
+++ training/training-solutions/src/main/java/debug/StarMain.java
@@ -0,0 +1,11 @@
+package debug;
+
+public class StarMain {
+
+    public static void main(String[] args) {
+        Star star = new Star();
+
+        int[] numbers = {1, 3, 6, 10, 15};
+        star.writeStars(numbers);
+    }
+}
